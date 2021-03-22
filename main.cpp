9a088005174b730375f49726cf034f5e2fd64e5b
Adam Ard <adam.ard2011@gmail.com>
2021-03-22 16:25:37 -0600

    added image to details

--- main.cpp
+++ main.cpp
@@ -20,39 +20,44 @@ int main(int argc, char *argv[])
 
    QLineEdit *symLineEdit= new QLineEdit;
 
-   QString msg = "These are the stock details. \nThere are lots of lines in here";
+   QString msg = "These are the stock details. \nThere are lots of lines in here\nThese are the stock details. \nThere are lots of lines in here\nThese are the stock details. \nThere are lots of lines in here\nThese are the stock details. \nThere are lots of lines in here\nThese are the stock details. \nThere are lots of lines in here\n ";
    QLabel detailsLabel(msg);
 
    QTableWidget *tv = new QTableWidget();
-   tv->setRowCount(10);
+   tv->setRowCount(3);
    tv->setColumnCount(5);
    tv->setSelectionBehavior(QAbstractItemView::SelectRows);
    tv->horizontalHeader()->hide();
    tv->horizontalHeader()->setStretchLastSection(true);
    tv->verticalHeader()->hide();
    tv->setShowGrid(false);
-   
-   QTableWidgetItem* applIcon = new QTableWidgetItem();
-   applIcon->setIcon(QIcon("test.png"));
 
    QLabel logoLabel;
-   QPixmap pic("test.png");
+   QPixmap pic("tesla.png");
    logoLabel.setPixmap(pic);
 
-   
+   QTableWidgetItem* applIcon = new QTableWidgetItem();
+   applIcon->setIcon(QIcon("apple.png"));
    tv->setItem(0,0, applIcon);
+
    tv->setItem(0,1, new QTableWidgetItem("AAPL"));
    tv->setItem(0,2, new QTableWidgetItem("Apple"));
    tv->setItem(0,3, new QTableWidgetItem("-100"));
    tv->setItem(0,4, new QTableWidgetItem("-5"));
 
-   tv->setItem(1,0, new QTableWidgetItem(""));
+   QTableWidgetItem* googleIcon = new QTableWidgetItem();
+   googleIcon->setIcon(QIcon("google.png"));
+
+   tv->setItem(1,0, googleIcon);
    tv->setItem(1,1, new QTableWidgetItem("GOOG"));
    tv->setItem(1,2, new QTableWidgetItem("Google"));
    tv->setItem(1,3, new QTableWidgetItem("-100"));
    tv->setItem(1,4, new QTableWidgetItem("-5"));
 
-   tv->setItem(2,0, new QTableWidgetItem(""));
+   QTableWidgetItem* teslaIcon = new QTableWidgetItem();
+   teslaIcon->setIcon(QIcon("tesla.png"));
+   
+   tv->setItem(2,0, teslaIcon);
    tv->setItem(2,1, new QTableWidgetItem("TSLA"));
    tv->setItem(2,2, new QTableWidgetItem("Tesla"));
    tv->setItem(2,3, new QTableWidgetItem("-100"));
@@ -64,7 +69,7 @@ int main(int argc, char *argv[])
    layout->addWidget(deleteButton, 1, 0);
    layout->addWidget(tv, 2, 0, 2, 2);
    layout->addWidget(&logoLabel, 4, 0);
-   layout->addWidget(&detailsLabel, 5, 0, 5, 2);
+   layout->addWidget(&detailsLabel, 4, 1);
 
 
    window->resize(750, 500);

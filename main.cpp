5a5777d55b0abb5880c1ce15a7ce5e4331aff2e7
Adam Ard <adam.ard2011@gmail.com>
2021-03-22 17:26:06 -0600

    added row selection

--- main.cpp
+++ main.cpp
@@ -10,19 +10,31 @@
 #include <QTableWidget>
 #include <QHeaderView>
 
+#include <cstdio>
+
 QLabel* logoLabel;
 QLabel* detailsLabel;
 QLineEdit* symLineEdit;
+QTableWidget* stockList;
 
 
-void setDetails(QString symbol, QString details) {
+void setDetails(QString symbol, QString details)
+{
    QPixmap pic(symbol + ".png");
    logoLabel->setPixmap(pic);
    detailsLabel->setText(details);
 }
 
-void addStock() {
-  setDetails(symLineEdit->text(), "asdfasdfasdf\nasdfasdfasdf\nasdfafsdasdf\nasdfasdfaef");
+void updateDetails()
+{
+  int curr = stockList->currentRow();
+  QTableWidgetItem* currItem = stockList->item(curr, 1);
+
+  setDetails(currItem->text(), "asdfasdfasdf\nasdfasdfasdf\nasdfafsdasdf\nasdfasdfaef");
+}
+
+void addStock()
+{
 }
 
 int main(int argc, char *argv[])
@@ -39,54 +51,55 @@ int main(int argc, char *argv[])
 
    detailsLabel = new QLabel;
 
-   QTableWidget *tv = new QTableWidget();
-   tv->setRowCount(3);
-   tv->setColumnCount(5);
-   tv->setSelectionBehavior(QAbstractItemView::SelectRows);
-   tv->horizontalHeader()->hide();
-   tv->horizontalHeader()->setStretchLastSection(true);
-   tv->verticalHeader()->hide();
-   tv->setShowGrid(false);
+   stockList = new QTableWidget();
+   stockList->setRowCount(3);
+   stockList->setColumnCount(5);
+   stockList->setSelectionBehavior(QAbstractItemView::SelectRows);
+   stockList->horizontalHeader()->hide();
+   stockList->horizontalHeader()->setStretchLastSection(true);
+   stockList->verticalHeader()->hide();
+   stockList->setShowGrid(false);
 
    logoLabel = new QLabel;
    setDetails("TSLA", msg);
    
    QTableWidgetItem* applIcon = new QTableWidgetItem();
    applIcon->setIcon(QIcon("AAPL.png"));
-   tv->setItem(0,0, applIcon);
+   stockList->setItem(0,0, applIcon);
 
-   tv->setItem(0,1, new QTableWidgetItem("AAPL"));
-   tv->setItem(0,2, new QTableWidgetItem("Apple"));
-   tv->setItem(0,3, new QTableWidgetItem("-100"));
-   tv->setItem(0,4, new QTableWidgetItem("-5"));
+   stockList->setItem(0,1, new QTableWidgetItem("AAPL"));
+   stockList->setItem(0,2, new QTableWidgetItem("Apple"));
+   stockList->setItem(0,3, new QTableWidgetItem("-100"));
+   stockList->setItem(0,4, new QTableWidgetItem("-5"));
 
    QTableWidgetItem* googleIcon = new QTableWidgetItem();
    googleIcon->setIcon(QIcon("GOOG.png"));
 
-   tv->setItem(1,0, googleIcon);
-   tv->setItem(1,1, new QTableWidgetItem("GOOG"));
-   tv->setItem(1,2, new QTableWidgetItem("Google"));
-   tv->setItem(1,3, new QTableWidgetItem("-100"));
-   tv->setItem(1,4, new QTableWidgetItem("-5"));
+   stockList->setItem(1,0, googleIcon);
+   stockList->setItem(1,1, new QTableWidgetItem("GOOG"));
+   stockList->setItem(1,2, new QTableWidgetItem("Google"));
+   stockList->setItem(1,3, new QTableWidgetItem("-100"));
+   stockList->setItem(1,4, new QTableWidgetItem("-5"));
 
    QTableWidgetItem* teslaIcon = new QTableWidgetItem();
    teslaIcon->setIcon(QIcon("TSLA.png"));
    
-   tv->setItem(2,0, teslaIcon);
-   tv->setItem(2,1, new QTableWidgetItem("TSLA"));
-   tv->setItem(2,2, new QTableWidgetItem("Tesla"));
-   tv->setItem(2,3, new QTableWidgetItem("-100"));
-   tv->setItem(2,4, new QTableWidgetItem("-5"));
+   stockList->setItem(2,0, teslaIcon);
+   stockList->setItem(2,1, new QTableWidgetItem("TSLA"));
+   stockList->setItem(2,2, new QTableWidgetItem("Tesla"));
+   stockList->setItem(2,3, new QTableWidgetItem("-100"));
+   stockList->setItem(2,4, new QTableWidgetItem("-5"));
 
    QGridLayout *layout = new QGridLayout;
    layout->addWidget(addButton, 0, 0);
    layout->addWidget(symLineEdit, 0, 1);
    layout->addWidget(deleteButton, 1, 0);
-   layout->addWidget(tv, 2, 0, 2, 2);
+   layout->addWidget(stockList, 2, 0, 2, 2);
    layout->addWidget(logoLabel, 4, 0);
    layout->addWidget(detailsLabel, 4, 1);
 
    QObject::connect(addButton, &QPushButton::clicked, addStock);
+   QObject::connect(stockList, &QTableWidget::itemSelectionChanged, updateDetails);
 
    window->resize(750, 500);
    window->setLayout(layout);
